fad390cbd8888045b1d5a4ade0b62c2b7231fe6d
gossi <github@gos.si>
2016-10-05 14:46:31 +0200

    initial package

---
+++ src/UnicyclingModule.php
@@ -0,0 +1,30 @@
+<?php
+namespace iuf\unicycling;
+
+use keeko\framework\foundation\AbstractModule;
+
+/**
+ * Iuf Unicycling
+ * 
+ * @license MIT
+ * @author gossi
+ */
+class UnicyclingModule extends AbstractModule {
+
+	/**
+	 */
+	public function install() {
+	}
+
+	/**
+	 */
+	public function uninstall() {
+	}
+
+	/**
+	 * @param mixed $from
+	 * @param mixed $to
+	 */
+	public function update($from, $to) {
+	}
+}
